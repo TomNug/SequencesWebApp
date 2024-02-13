a1570bbaf90c2e13f7dc3d39c1a97279db70a42c
t.nugent <t.nugent@hotmail.co.uk>
2024-02-13 11:46:54 +0000

    DbContext, Entity, DB Connection

---
+++ SequencesWebApp/Migrations/20240213113515_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SequencesWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sequences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Integers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsAscending = table.Column<bool>(type: "bit", nullable: false),
+                    SortingTime = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sequences", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sequences");
+        }
+    }
+}
