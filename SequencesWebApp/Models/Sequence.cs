0ca456f44a8a69c6fa4166799383be95d0d43b28
t.nugent <t.nugent@hotmail.co.uk>
2024-02-13 11:28:13 +0000

    Model

---
+++ SequencesWebApp/Models/Sequence.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SequencesWebApp.Models
+{
+    public class Sequence
+    {
+        [Key]
+        public int Id { get; set; }
+        public List<int> Integers { get; set; }
+        public bool IsAscending { get; set; }
+        public float SortingTime { get; set; }
+    }
+}
