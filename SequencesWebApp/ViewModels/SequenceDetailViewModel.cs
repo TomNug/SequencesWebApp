2a0fa72b12601c0fa41b5d12d91458fab71e5791
t.nugent <t.nugent@hotmail.co.uk>
2024-02-19 17:01:19 +0000

    Default value for nullable prop

--- SequencesWebApp/ViewModels/SequenceDetailViewModel.cs
+++ SequencesWebApp/ViewModels/SequenceDetailViewModel.cs
@@ -5,7 +5,7 @@ namespace SequencesWebApp.ViewModels
     public class SequenceDetailViewModel
     {
         public int Id { get; set; }
-        public List<SequenceInt> Integers { get; set; }
+        public List<SequenceInt> Integers { get; set; } = new List<SequenceInt>();
         public bool IsAscending { get; set; }
         public float SortingTime { get; set; }
     }
